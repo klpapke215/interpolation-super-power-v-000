6ea64c3d4cd27674629a29c35347c32a12f84087
Kristen Papke <klpapke215@gmail.com>
2018-05-01 20:04:09 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow (ROYGBIV)
+def display_rainbow (roygbiv)
   puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
