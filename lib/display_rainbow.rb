e3bf72488f3546881323fb662ab4293a2a03be14
Kristen Papke <klpapke215@gmail.com>
2018-05-01 19:47:49 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow
-  puts "ROYGBV #{red[0], Orange[1], Yellow[2], Green[3], Blue[4], Indigo[5], Violet[6]}"
+  puts "ROYGBIV #{red[0], orange[1], yellow[2], green[3], blue[4], indigo[5], violet[6]}"
 end
